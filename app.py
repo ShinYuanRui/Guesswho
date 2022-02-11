d25c4da1586682aad51d27554cd23b9fe7d300aa
SWR <1352278266@qq.com>
2022-02-11 23:33:07 +0800

    hello page change

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    def hello():
-        return '<h1>Hello Totoro!</h1><img src="http://helloflask.com / totoro.gif">'
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com / totoro.gif">'
 @app.route('/user/<name>')
 def user_page(name):
     return 'User: %s' % name
