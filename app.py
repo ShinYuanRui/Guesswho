4dcd72f9997e5d7b234a2ab5f8f43c264ff5cd43
SWR <1352278266@qq.com>
2022-03-13 23:17:53 +0800

    database about login and regist without avatars

--- app.py
+++ app.py
@@ -1,25 +1,156 @@
-from flask import url_for, Flask, render_template
-
-app = Flask(__name__)
+import os,sys
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
+from sqlalchemy import and_, or_
 
 name = 'Rui'
 
-@app.route('/')
-def login():
-    return render_template('login.html', name=name)
+app = Flask(__name__)
 
+# basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', prefix + os.path.join(app.root_path, 'data.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# 定义ORM
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+# 创建表格、插入数据
+@app.before_first_request
+def create_db():
+    db.drop_all()  # 每次运行，先删除再创建
+    db.create_all()
+
+    admin = User(username='admin', password='root')
+    db.session.add(admin)
+
+    guestes = [User(username='guest1', password='guest1'),
+               User(username='guest2', password='guest2'),
+               User(username='guest3', password='guest3'),
+               User(username='guest4', password='guest4')]
+    db.session.add_all(guestes)
+    db.session.commit()
+
+############################################
+# 辅助函数、装饰器
+############################################
+
+# 登录检验（用户名、密码验证）
+def valid_login(username, password):
+    user = User.query.filter(and_(User.username == username, User.password == password)).first()
+    if user:
+        return True
+    else:
+        return False
+
+# 注册检验（用户名、邮箱验证）
+def valid_regist(username):
+    user = User.query.filter(User.username == username).first()
+    if user:
+        return False
+    else:
+        return True
+
+# 登录
+def login_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # if g.user:
+        if session.get('username'):
+            return func(*args, **kwargs)
+        else:
+            return redirect(url_for('login', next=request.url))
+    return wrapper
+
+
+############################################
+# 路由
+############################################
+
+# 1.主页
+@app.route('/')
+def home():
+    return render_template('home.html', name=name)
+
+
+# 2.登录
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['login_user'], request.form['pswd']):
+            flash("成功登录！")
+            session['username'] = request.form.get('login_user')
+            return redirect(url_for('index'))
+        else:
+            error = '错误的用户名或密码！'
+
+    return render_template('login.html', error=error)
+
+
+# 3.注销
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+
+# 4.注册
+@app.route('/regist', methods=['GET', 'POST'])
+def regist():
+    error = None
+    if request.method == 'POST':
+        if request.form['pswd1'] != request.form['pswd2']:
+            error = '两次密码不相同！'
+        elif valid_regist(request.form['register_user']):
+            user = User(username=request.form['register_user'], password=request.form['pswd1'])
+            db.session.add(user)
+            db.session.commit()
+
+            flash("成功注册！")
+            return redirect(url_for('login'))
+        else:
+            error = '该用户名或邮箱已被注册！'
+
+    return render_template('regist.html', error=error)
+
+# @app.route('/')
+# def home():
+#     return render_template('home.html', name=name)
+#
+# @app.route('/login')
+# def login():
+#     return render_template('login.html', name=name)
+#
 @app.route('/game')
 def index():
     return render_template('index.html', name=name)
-
-@app.route('/register')
-def register():
-    return render_template('register.html', name=name)
+#
+# @app.route('/register')
+# def register():
+#     return render_template('regist.html', name=name)
 
 @app.route('/user/<name>')
 def user_page(name):
     return 'User: %s' % name
-
 
 if __name__ == '__main__':
     app.run()
--- static/alert.js
+++ static/alert.js
@@ -1,131 +1,42 @@
-(function($) {        
-    $.alerts = {         
-        alert: function(title, message, callback) {  
-            if( title == null ) title = 'Alert';  
-            $.alerts._show(title, message, null, 'alert', function(result) {  
-                if( callback ) callback(result);  
-            });  
-        },  
-           
-        confirm: function(title, message, callback) {  
-            if( title == null ) title = 'Confirm';  
-            $.alerts._show(title, message, null, 'confirm', function(result) {  
-                if( callback ) callback(result);  
-            });  
-        },  
-               
-          
-        _show: function(title, msg, value, type, callback) {  
-            
-                    var _html = "";  
-   
-                    _html += '  <div class="alert alert-danger alert-dismissible fade show">';  
-                    _html += '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';  
-                      if (type == "alert") {  
-						_html += '<strong>False!</strong>'+msg; 
-						_html += '</div>';					  
-                    }  
-                    if (type == "confirm") {  
-                      _html += '<input id="mb_btn_no" type="button" value="取消" />';  
-                      _html += '<input id="mb_btn_ok" type="button" value="确定" />';
-					  _html += '</div></div>'; 
-                    }  
-                      
-                   
-                    //必须先将_html添加到body，再设置Css样式  
-                    $("body").append(_html); GenerateCss();  
-           
-            switch( type ) {  
-                case 'alert':  
-          
-                    $("#mb_btn_ok").click( function() {  
-                        $.alerts._hide();  
-                        callback(true);  
-                    });  
-                    $("#mb_btn_ok").focus().keypress( function(e) {  
-                        if( e.keyCode == 13 || e.keyCode == 27 ) $("#mb_btn_ok").trigger('click');  
-                    });  
-                break;  
-                case 'confirm':  
-                     
-                    $("#mb_btn_ok").click( function() {  
-                        $.alerts._hide();  
-                        if( callback ) callback(true);  
-                    });  
-                    $("#mb_btn_no").click( function() {  
-                        $.alerts._hide();  
-                        if( callback ) callback(false);  
-                    });  
-                    $("#mb_btn_no").focus();  
-                    $("#mb_btn_ok, #mb_btn_no").keypress( function(e) {  
-                        if( e.keyCode == 13 ) $("#mb_btn_ok").trigger('click');  
-                        if( e.keyCode == 27 ) $("#mb_btn_no").trigger('click');  
-                    });  
-                break;
-            }  
-        },  
-        _hide: function() {  
-             $("#mb_box,#mb_con").remove();  
-        }  
-    }  
-    // Shortuct functions  
-    myAlert = function(title, message, callback) {  
-        $.alerts.alert(title, message, callback);  
-    }  
-       
-    myConfirm = function(title, message, callback) {  
-        $.alerts.confirm(title, message, callback);  
-    };  
-           
-   
-      
-      //生成Css  
-  var GenerateCss = function () {  
-   
-    $("#mb_box").css({ width: '100%', height: '100%', zIndex: '99999', position: 'fixed',  
-      filter: 'Alpha(opacity=60)', backgroundColor: 'black', top: '0', left: '0', opacity: '0.6'  
-    });  
-   
-    $("#mb_con").css({ zIndex: '999999', width: '350px',height:'200px', position: 'fixed',  
-      backgroundColor: 'White',  
-    });  
-   
-    $("#mb_tit").css({ display: 'block', fontSize: '14px', color: '#444', padding: '10px 15px',  
-      backgroundColor: '#fff', borderRadius: '15px 15px 0 0',  
-      fontWeight: 'bold'  
-    });  
-   
-    $("#mb_msg").css({ padding: '20px', lineHeight: '40px', textAlign:'center', 
-      fontSize: '18px' ,color:'#4c4c4c' 
-    });  
-   
-    $("#mb_ico").css({ display: 'block', position: 'absolute', right: '10px', top: '9px',  
-      border: '1px solid Gray', width: '18px', height: '18px', textAlign: 'center',  
-      lineHeight: '16px', cursor: 'pointer', borderRadius: '12px', fontFamily: '微软雅黑'  
-    });  
-   
-    $("#mb_btnbox").css({ margin: '15px 0px 10px 0', textAlign: 'center' });  
-    $("#mb_btn_ok,#mb_btn_no").css({ width: '80px', height: '30px', color: 'white', border: 'none', borderRadius:'4px'});  
-    $("#mb_btn_ok").css({ backgroundColor: '#41a259' });  
-    $("#mb_btn_no").css({ backgroundColor: 'gray', marginRight: '40px' });  
-   
-   
-    //右上角关闭按钮hover样式  
-    $("#mb_ico").hover(function () {  
-      $(this).css({ backgroundColor: 'Red', color: 'White' });  
-    }, function () {  
-      $(this).css({ backgroundColor: '#DDD', color: 'black' });  
-    });  
-   
-    var _widht = document.documentElement.clientWidth; //屏幕宽  
-    var _height = document.documentElement.clientHeight; //屏幕高  
-   
-    var boxWidth = $("#mb_con").width();  
-    var boxHeight = $("#mb_con").height();  
-   
-    //让提示框居中  
-    $("#mb_con").css({ top: (_height - boxHeight) / 2 + "px", left: (_widht - boxWidth) / 2 + "px" });  
-  }  
-   
-  
-})(jQuery);
+/**
+ * 弹出式提示框，默认1.2秒自动消失
+ * @param message 提示信息
+ * @param style 提示样式，有alert-success、alert-danger、alert-warning、alert-info
+ * @param time 消失时间
+ */
+var prompt = function (message, style, time)
+{
+    style = (style === undefined) ? 'alert-success' : style;
+    time = (time === undefined) ? 1200 : time;
+    $('<div>')
+        .appendTo('body')
+        .addClass('alert ' + style)
+        .html(message)
+        .show()
+        .delay(time)
+        .fadeOut();
+};
+
+// 成功提示
+var success_prompt = function(message, time)
+{
+    prompt(message, 'alert-success', time);
+};
+
+// 失败提示
+var fail_prompt = function(message, time)
+{
+    prompt(message, 'alert-danger', time);
+};
+
+// 提醒
+var warning_prompt = function(message, time)
+{
+    prompt(message, 'alert-warning', time);
+};
+
+// 信息提示
+var info_prompt = function(message, time)
+{
+    prompt(message, 'alert-info', time);
+};
